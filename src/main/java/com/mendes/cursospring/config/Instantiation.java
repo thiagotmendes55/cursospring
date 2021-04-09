550787b3a4cd1ed4f239c8cac193debe7b00eaaf
thiagotmendes <thiagotmendes@gmail.com>
2021-04-09 15:18:37 -0300

    Classe Produto e associacao muitos para muitos

--- src/main/java/com/mendes/cursospring/config/Instantiation.java
+++ src/main/java/com/mendes/cursospring/config/Instantiation.java
@@ -7,7 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.mendes.cursospring.domain.Categoria;
+import com.mendes.cursospring.domain.Produto;
 import com.mendes.cursospring.repositories.CategoriaRepository;
+import com.mendes.cursospring.repositories.ProdutoRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -15,12 +17,27 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		Categoria c1 = new Categoria(null, "Informática");
 		Categoria c2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		c1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		c2.getProdutos().add(p2);
+		
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1, c2));		
+		p3.getCategorias().addAll(Arrays.asList(c1));
+		
 		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
