e2e84d5094955fe565e914e7a0cfeaacf71cb886
thiagotmendes <thiagotmendes@gmail.com>
2021-04-09 22:23:13 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/mendes/cursospring/config/Instantiation.java
+++ src/main/java/com/mendes/cursospring/config/Instantiation.java
@@ -64,6 +64,13 @@ public class Instantiation implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria c1 = new Categoria(null, "Informática");
 		Categoria c2 = new Categoria(null, "Escritório");
+		Categoria c3 = new Categoria(null, "Cama, Mesa e Banho");
+		Categoria c4 = new Categoria(null, "Eletrônicos");
+		Categoria c5 = new Categoria(null, "Jardinagem");
+		Categoria c6 = new Categoria(null, "Decoração");
+		Categoria c7 = new Categoria(null, "Perfumaria");
+		Categoria c8 = new Categoria(null, "Eletrodomésticos");
+		Categoria c9 = new Categoria(null, "Televisores");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -76,7 +83,7 @@ public class Instantiation implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(c1, c2));		
 		p3.getCategorias().addAll(Arrays.asList(c1));
 		
-		categoriaRepository.saveAll(Arrays.asList(c1, c2));
+		categoriaRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6, c7, c8, c9));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado e1 = new Estado(null, "Minas Gerais");
