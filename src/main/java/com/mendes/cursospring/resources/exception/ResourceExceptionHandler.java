a78cd125d39c7eb4992328c2464b47d68cb989dc
thiagotmendes <thiagotmendes@gmail.com>
2021-04-09 21:51:56 -0300

    Excluindo uma categoria com DELETE

--- src/main/java/com/mendes/cursospring/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/mendes/cursospring/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.mendes.cursospring.services.exception.DataIntegrityException;
 import com.mendes.cursospring.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -16,6 +17,13 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {		
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Objeto não encontrado", e.getMessage(), request.getRequestURI());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Erro na integridade de dados", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
 	}
 }
--- src/main/java/com/mendes/cursospring/services/CategoriaService.java
+++ src/main/java/com/mendes/cursospring/services/CategoriaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.mendes.cursospring.domain.Categoria;
 import com.mendes.cursospring.repositories.CategoriaRepository;
+import com.mendes.cursospring.services.exception.DataIntegrityException;
 import com.mendes.cursospring.services.exception.ObjectNotFoundException;
 
 @Service
@@ -15,16 +17,16 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repositorio;
-	
+
 	public List<Categoria> findAll() {
 		return repositorio.findAll();
 	}
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repositorio.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repositorio.save(obj);
@@ -32,7 +34,16 @@ public class CategoriaService {
 
 	public Categoria update(Categoria obj) {
 		this.findById(obj.getId());
-		
+
 		return repositorio.save(obj);
+	}
+
+	public void delete(Integer id) {
+		this.findById(id);
+		try {
+			repositorio.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que tem produtos!");
+		}
 	}
 }
