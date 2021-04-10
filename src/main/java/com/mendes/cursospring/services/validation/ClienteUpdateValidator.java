4137c0703bea931ff3cfc63ab172d9701fcbfa08
thiagotmendes <thiagotmendes@gmail.com>
2021-04-10 16:25:15 -0300

    Validação customizada não aceita 2 emails iguais no Update do Cliente

---
+++ src/main/java/com/mendes/cursospring/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,50 @@
+package com.mendes.cursospring.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.mendes.cursospring.domain.Cliente;
+import com.mendes.cursospring.dto.ClienteDTO;
+import com.mendes.cursospring.repositories.ClienteRepository;
+import com.mendes.cursospring.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repositorio;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		Cliente aux = repositorio.findByEmail(objDto.getEmail());
+		if ((aux != null) && (!(aux.getId().equals(uriId)))) {
+			list.add(new FieldMessage("email", "Email já cadastrado!"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
