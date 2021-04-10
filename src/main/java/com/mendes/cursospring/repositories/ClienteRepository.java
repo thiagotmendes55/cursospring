fc5fc660f095e7b2d6297bfb23df3c5716bb1ace
thiagotmendes <thiagotmendes@gmail.com>
2021-04-10 16:11:42 -0300

    Validação customizada não aceita 2 emails iguais no Insert do Cliente

--- src/main/java/com/mendes/cursospring/repositories/ClienteRepository.java
+++ src/main/java/com/mendes/cursospring/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.mendes.cursospring.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mendes.cursospring.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly = true)
+	public Cliente findByEmail(String email);
+	
 }
--- src/main/java/com/mendes/cursospring/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/mendes/cursospring/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mendes.cursospring.domain.Cliente;
 import com.mendes.cursospring.domain.enums.TipoCliente;
 import com.mendes.cursospring.dto.ClienteNewDTO;
+import com.mendes.cursospring.repositories.ClienteRepository;
 import com.mendes.cursospring.resources.exception.FieldMessage;
 import com.mendes.cursospring.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repositorio;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if ((objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo())) && (!(BR.isCNPJ(objDto.getCpfOuCnpj())))) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		Cliente aux = repositorio.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado!"));
 		}
 		
 		for (FieldMessage e : list) {
