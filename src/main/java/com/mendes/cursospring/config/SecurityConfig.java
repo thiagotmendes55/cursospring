0fd8930f18bcb1438c7aeedba476b1b5780bcd94
thiagotmendes <thiagotmendes@gmail.com>
2021-04-11 13:10:53 -0300

    Implementando autorização

--- src/main/java/com/mendes/cursospring/config/SecurityConfig.java
+++ src/main/java/com/mendes/cursospring/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mendes.cursospring.security.JWTAuthenticationFilter;
+import com.mendes.cursospring.security.JWTAuthorizationFilter;
 import com.mendes.cursospring.security.JWTUtil;
 
 @Configuration
@@ -48,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
